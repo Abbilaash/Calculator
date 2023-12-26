d6d1304b734684178de55927513a3849c6d1fedf
Abbilaash <abbilaashat@gmail.com>
2023-12-26 23:32:49 +0530

    third commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# importing all modules
+import customtkinter 
+from tkinter import *
+import func
+
+
+# Starting the tk windows
+window = customtkinter.CTk()
+window.geometry("450x600")
+window.title("Calculator")
+window.resizable(width=0,height=0)
+window.configure(fg_color='gray')
+
+def App():
+    pass
+
+
+
+
+
+
+
+App()
+
+window.mainloop()
