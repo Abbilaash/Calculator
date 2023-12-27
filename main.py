106f56132b9d2a3ac6d2a824b32effe0379c6dfd
Abbilaash <abbilaashat@gmail.com>
2023-12-28 00:36:36 +0530

    fourth commit

--- main.py
+++ main.py
@@ -12,7 +12,16 @@ window.resizable(width=0,height=0)
 window.configure(fg_color='gray')
 
 def App():
-    pass
+    equation_entry = customtkinter.CTkEntry(master=window,width=440,height=50,justify="right")
+    equation_entry.place(x=5,y=5)
+
+    # define the function for calculation
+    def Calculate(eqn):
+        pass
+    
+     
+    main_calc_entry = customtkinter.CTkEntry(master=window,font=("Helvetica",40),text_color="gray20",width=440,justify="right",height=80,border_color="white",border_width=1,fg_color="gray")
+    main_calc_entry.place(x=5,y=55)
 
 
 
